eda602b313325343e892e5bfb2d88cc277425291
unknown <raje1reddy@gmail.com>
2019-08-19 17:54:50 +0100

    Passing primitives to methods

---
+++ src/main/java/Employee.java
@@ -0,0 +1,11 @@
+
+class Employee {
+    String name;
+    int age;
+    Employee(){
+        age = 20;
+        System.out.println("user defined constructor");
+
+    }
+}
+
---
+++ src/test/java/EmployeeTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+
+public class EmployeeTest {
+    public static void main (String args[]) {
+        Employee emp = new Employee();
+        System.out.println(emp.age);
+    }
+
+}
